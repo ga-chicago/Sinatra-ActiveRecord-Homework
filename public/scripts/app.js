1b89f9c1b3056c98c29c525224265c1fe63cf48d
Bridjet Mendyuk <bjmendy@Bridjets-MacBook-Pro.local>
2017-03-27 11:20:56 -0500

    updated again 11am

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -19,3 +19,39 @@ $.ajax({
     console.log(err)
   }
 })
+
+
+$('.delete').on('click', function(e){
+  var id = $(e.target).data('id')
+  $a.jax({
+    url: '/candy' + id
+    type: 'DELETE',
+    success: function(result){
+      console.log(result)
+    }
+  })
+})
+
+$('.update').on('click', function(e){
+  var id = $(e.target).data('id');
+  var type = $(e.target).closest('li').find('.type').val;
+  var sweetness = $(e.target).closest('li').find('.sweetness').val;
+  var price = $(e.target).closest('li').find('.price').val;
+  var health = $(e.target).closest('li').find('.health').val;
+  console.log(name);
+  $a.jax({
+    url: '/candy' + id
+    type: 'PATCH',
+    data: {
+      type: type,
+      sweetness: sweetness,
+      price: price,
+      health: health
+
+    },
+
+success: function(response){
+  console.log(response);
+}
+
+  })
