1a2308f0c9cf81bff225497281fb87439ec0a759
Collin Cromwell <collcrom@gmail.com>
2017-11-07 17:23:04 -0600

    cant set headers in my ajax call to work and not sure how ERB works tbh

--- controllers/AppController.rb
+++ controllers/AppController.rb
@@ -2,6 +2,8 @@ class AppController < Sinatra::Base
 	require 'bundler'
 	Bundler.require
 
+	set :views, File.expand_path('../../views', __FILE__)
+
 	ActiveRecord::Base.establish_connection(
 		:adapter => 'postgresql',
 		:database => 'pet'
